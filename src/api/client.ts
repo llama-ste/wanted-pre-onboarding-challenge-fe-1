427cb2f066e894579d7aca722b8012c1c8ef7165
llama-ste <andong1212@gmail.com>
2023-01-05 13:38:45 +0900

    refactor: axios interceptor로 에러 핸들링

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,7 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+import { IError } from "../types/axios";
+import { toast } from "react-toastify";
+import customHistory from "../utils/history";
 
 const baseURL: string = process.env.REACT_APP_BASE_URL as string;
 
@@ -15,6 +18,18 @@ client.interceptors.request.use(
     return config;
   },
   (error) => Promise.reject(error)
+);
+
+client.interceptors.response.use(
+  (res: AxiosResponse) => res,
+  (error: AxiosError<IError>) => {
+    const isTokenMissing = error.response?.data.details === "Token is missing";
+
+    if (isTokenMissing) {
+      toast.warning("로그인을 해주세요.");
+      customHistory.replace("/auth");
+    }
+  }
 );
 
 export default client;
